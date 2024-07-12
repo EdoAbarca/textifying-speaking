706bf86725efe24ae139134f675de466699bdd59
Eduardo Abarca <eduardo.abarca.c@usach.cl>
2024-07-11 22:42:44 -0400

    CRUD básico User (temporal)

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MethodsModule } from './methods/methods.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,8 +11,7 @@ import { MethodsModule } from './methods/methods.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
     MethodsModule,
+    UserModule,
   ],
-  //controllers: [AppController],
-  //providers: [AppService],
 })
 export class AppModule {}
