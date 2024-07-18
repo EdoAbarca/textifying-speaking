5b22f9d83c8865f7c9f530aaa944642a2a692577
Eduardo Abarca <eduardo.abarca.c@usach.cl>
2024-07-18 16:33:35 -0400

    feat: Se implementan las funcionalidades y Endpoints a usar en la capa de la vista

--- nest-backend/src/methods/methods.controller.ts
+++ nest-backend/src/methods/methods.controller.ts
@@ -1,10 +1,55 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MethodsService } from './methods.service';
+import { CreateTranscriptionDto } from 'src/transcription/dto/create-transcription.dto';
+import { CreateKeyDto } from 'src/key/dto/create-key.dto';
 
 
 @Controller('methods')
 export class MethodsController {
   constructor(private readonly methodsService: MethodsService) {}
 
+  @Get('transcriptor-models')
+  getTranscriptorModels() {
+    return this.methodsService.getTranscriptorModels();
+  }
 
+  @Get('summarizer-models')
+  getSummarizerModels() {
+    return this.methodsService.getSummarizerModels
+  }
+
+  @Get('transcriptions')
+  getTranscriptions() {
+    return this.methodsService.getTranscriptions();
+  }
+
+  @Get('transcriptor-keys')
+  getTranscriptorKeys() {
+    return this.methodsService.getTranscriptorsAPIKeys();
+  }
+
+  @Get('summarizer-keys')
+  getSummarizerKeys() {
+    return this.methodsService.getSummarizersAPIKeys();
+  }
+
+  @Post('create-key')
+  createKey(@Body() createKeyDto: CreateKeyDto) {
+    return this.methodsService.createKey(createKeyDto);
+  }
+
+  @Post('create-transcription')
+  createTranscription(@Body() createTranscriptionDto: CreateTranscriptionDto) {
+    return this.methodsService.createTranscription(createTranscriptionDto);
+  }
+
+  @Delete('remove-transcription/:id')
+  removeTranscription(@Param('id') id: string) {
+    return this.methodsService.removeTranscription(id);
+  }
+
+  @Delete('remove-key/:id')
+  removeKey(@Param('id') id: string) {
+    return this.methodsService.removeKey(id);
+  }
 }
